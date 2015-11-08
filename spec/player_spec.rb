cafdd31d1792df0f9f31f933e02d902e74c01d5f
keithconroy <kconroyjr@gmail.com>
2015-11-08 14:43:23 -0800

    add player #initialize method and tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,19 @@
 require "spec_helper"
+
+describe Player do
+  let(:player){ Player.new }
+
+  context "#initialize" do
+    it "should be a Player" do
+      expect(player).to be_a(Player)
+    end
+    it "should have a cards attribute" do
+      expect(player.cards).to be_a(Array)
+    end
+    it "should be able to push to cards" do
+      player.cards << "2"
+      expect(player.cards).to eq ["2"]
+    end
+  end
+
+end
