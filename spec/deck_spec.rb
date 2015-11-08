2436f4f38200355898f90921d4f850f3f7ca19ee
keithconroy <kconroyjr@gmail.com>
2015-11-08 14:22:20 -0800

    add deck #initialize and tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,1 +1,25 @@
 require "spec_helper"
+
+describe Deck do
+  let(:deck){ Deck.new }
+  let(:cards) {
+    [
+      ["2","3","4","5","6","7","8","9","10","J","K","Q","A"],
+      ["2","3","4","5","6","7","8","9","10","J","K","Q","A"],
+      ["2","3","4","5","6","7","8","9","10","J","K","Q","A"],
+      ["2","3","4","5","6","7","8","9","10","J","K","Q","A"],
+    ].flatten
+  }
+
+  context "#initialize" do
+    it "should be a Deck" do
+      expect(deck).to be_a(Deck)
+    end
+    it "should have an array of cards" do
+      expect(deck.cards).to eq(cards)
+    end
+    it "should have 52 cards" do
+      expect(deck.cards.count).to eq 52
+    end
+  end
+end
