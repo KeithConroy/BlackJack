2cd293962e97ed3ab48c3d6b7bd29481ad0beeb7
keithconroy <kconroyjr@gmail.com>
2015-11-08 17:43:00 -0800

    add #hit, #highest_score and #winner

--- app/game.rb
+++ app/game.rb
@@ -12,27 +12,42 @@ class Game
     @cards = (decks.map {|deck| deck.cards}).flatten
     @dealer = Player.new({name: "Dealer"})
     @players = []
-    args[:players].times {|i| @players << Player.new({name: "Player#{i+1}"})}
+    args[:players].times {|i| @players << Player.new({name: "Player#{i+1}", dealer: @dealer})}
 
     @card_count = 0
   end
 
   def deal
     @cards.shuffle!
-    2.times do
-      card = @cards.pop
-      @card_count += Blackjack.count_cards(card)
-      # p "count #{@card_count}"
-      @dealer.cards << card
-    end
+
+    card = @cards.pop
+    @card_count += Blackjack.count_cards(card)
+    @dealer.cards << card
+
     @players.each do |player|
       2.times do
         card = @cards.pop
         @card_count += Blackjack.count_cards(card)
-        # p "count #{@card_count}"
         player.cards << card
       end
     end
+  end
+
+  def hit(player)
+    card = @cards.pop
+    @card_count += Blackjack.count_cards(card)
+    player.cards << card
+  end
+
+  def highest_score
+    in_it = @players.select {|player| !player.bust?}
+    in_it.max_by{|player| player.total}.total
+  end
+
+  def winner
+    everyone = [@dealer, @players].flatten
+    in_it = everyone.select {|player| !player.bust?}
+    in_it.max_by{|player| player.total}
   end
 
 end
