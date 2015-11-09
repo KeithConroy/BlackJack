db5c5046568dd9530f0a3c63afe2a41018c9b1bb
keithconroy <kconroyjr@gmail.com>
2015-11-08 16:29:02 -0800

    add player names and card counting

--- app/game.rb
+++ app/game.rb
@@ -1,17 +1,18 @@
 require_relative "deck"
 require_relative "player"
+require_relative "blackjack"
 
 class Game
   attr_reader :cards, :dealer, :players, :card_count
 
-  def initialize(args)
+  def initialize(args = {})
     decks = []
     args[:decks].times {decks << Deck.new}
 
     @cards = (decks.map {|deck| deck.cards}).flatten
-    @dealer = Player.new
+    @dealer = Player.new({name: "Dealer"})
     @players = []
-    args[:players].times {@players << Player.new}
+    args[:players].times {|i| @players << Player.new({name: "Player#{i+1}"})}
 
     @card_count = 0
   end
@@ -19,11 +20,17 @@ class Game
   def deal
     @cards.shuffle!
     2.times do
-      @dealer.cards << @cards.pop
+      card = @cards.pop
+      @card_count += Blackjack.count_cards(card)
+      # p "count #{@card_count}"
+      @dealer.cards << card
     end
     @players.each do |player|
       2.times do
-        player.cards << @cards.pop
+        card = @cards.pop
+        @card_count += Blackjack.count_cards(card)
+        # p "count #{@card_count}"
+        player.cards << card
       end
     end
   end
