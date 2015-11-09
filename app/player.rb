4e7fbe99b27609869c2989e6d81ffa8fccc03524
keithconroy <kconroyjr@gmail.com>
2015-11-08 17:40:47 -0800

    add blackjack?, @dealer and #stay?

--- app/player.rb
+++ app/player.rb
@@ -6,14 +6,33 @@ class Player
   def initialize(args)
     @name = args[:name]
     @cards = []
+    @dealer = args[:dealer]
   end
 
   def bust?
     total > 21
   end
 
+  def blackjack?
+    total == 21
+  end
+
   def total
     Blackjack.total(@cards)
+  end
+
+  def stay?
+    if total <= 11
+      return false
+    elsif total <= 16
+      if @dealer.total <= 6
+        return true
+      else
+        return false
+      end
+    else total >= 17
+      return true
+    end
   end
 
 end
